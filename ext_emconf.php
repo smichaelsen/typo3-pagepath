5532eecae1bc773d984c238fd6b0e60685eb8185
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-02-15 15:25:45 +0100

    [TASK] Test against TYPO3 8.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => [
         'depends' =>
             [
-                'typo3' => '7.6.2-8.5.99',
+                'typo3' => '7.6.2-8.6.99',
                 'php' => '5.5.0-7.0.99',
             ],
     ],
